c0106c36ceebe4575f0ef6adb4a82664765e644c
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-05-15 11:41:21 -0300

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./css/global.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Analytics>
+      <App />
+    </Analytics>
   </React.StrictMode>
 );
